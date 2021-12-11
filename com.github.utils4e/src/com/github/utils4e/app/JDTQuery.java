7e13957ae9fd0ba18af04ea0a362e2b4a2f1aa87
Roland Grunberg <rgrunber@redhat.com>
2021-12-10 22:29:24 -0500

    Add very basic JDT AST Viewing capability
    - Use first argument to switch on corresponding functionality
    - 'index', 'indexer' for JDT indexing
    - 'ast' for JDT AST
    
    Signed-off-by: Roland Grunberg <rgrunber@redhat.com>

--- com.github.utils4e/src/com/github/utils4e/app/JDTQuery.java
+++ com.github.utils4e/src/com/github/utils4e/app/JDTQuery.java
@@ -27,9 +27,16 @@ import static org.eclipse.jdt.internal.core.search.indexing.IIndexConstants.REF;
 import static org.eclipse.jdt.internal.core.search.indexing.IIndexConstants.SUPER_REF;
 import static org.eclipse.jdt.internal.core.search.indexing.IIndexConstants.TYPE_DECL;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.index.JavaIndexer;
 import org.eclipse.jdt.core.search.SearchPattern;
 import org.eclipse.jdt.internal.core.index.EntryResult;
@@ -44,8 +51,57 @@ public class JDTQuery {
 			MODULE_INFO };
 
 	public void executeQuery(String [] args) {
-		String pathToJar = args[0];
-		String pathToIndex = args[1];
+		switch (args[0]) {
+		case "index":
+		case "indexer":
+			performIndexing(args);
+			break;
+		case "ast":
+			performPrintAST(args);
+			break;
+		default:
+			break;
+		}
+	}
+
+	private void performPrintAST(String[] args) {
+		try (BufferedReader buff = new BufferedReader(new FileReader(new File(args[1])))) {
+			ASTParser parser = ASTParser.newParser(AST.getJLSLatest());
+			char [] cbuf = new char [2048];
+			StringBuilder source = new StringBuilder();
+			while(buff.read(cbuf) > 0) {
+				source.append(cbuf);
+				cbuf = new char [2048];
+			}
+			parser.setSource(source.toString().toCharArray());
+			CompilationUnit cuRoot = (CompilationUnit) parser.createAST(null);
+
+			ASTVisitor visitor = new ASTVisitor() {
+				@Override
+				public void preVisit(ASTNode node) {
+					int level = getLevel(node);
+					System.out.println("=".repeat(level) + ">" + node.getClass());
+					super.preVisit(node);
+				}
+
+				private int getLevel(ASTNode node) {
+					int lvl = 0;
+					ASTNode curr = node;
+					while (curr.getParent() != null) {
+						lvl++;
+						curr = curr.getParent();
+					}
+					return lvl;
+				}
+			};
+			cuRoot.accept(visitor);
+		} catch (IOException e) {
+		}
+	}
+
+	private void performIndexing(String[] args) {
+		String pathToJar = args[1];
+		String pathToIndex = args[2];
 		try {
 			JavaIndexer.generateIndexForJar(pathToJar, pathToIndex);
 			IndexLocation indexLocation = new FileIndexLocation(new File(pathToIndex));
@@ -63,7 +119,6 @@ public class JDTQuery {
 				}
 			}
 		} catch (IOException e) {
-			e.printStackTrace();
 		}
 	}
 }
